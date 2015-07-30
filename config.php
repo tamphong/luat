5bebe8726beebabc191e663ddc6fa9ba32fb4fd1
sdfsdfsdftamphong <tranduyphong1407@gmail.com>
2015-07-30 14:32:55 +0700

    ok
    ok

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @Project NUKEVIET 3.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2015 VINADES.,JSC. All rights reserved
+ * @Createdate Thu, 23 Jul 2015 08:49:42 GMT
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+    die( 'Stop!!!' );
+}
+
+$db_config['dbhost'] = "localhost";
+$db_config['dbport'] = "";
+$db_config['dbname'] = "luat";
+$db_config['dbuname'] = "root";
+$db_config['dbpass'] = "";
+$db_config['prefix'] = "nv3";
+
+$global_config['sitekey'] = "d9d7bf96076d22f17f5d07b01ce55e88";// Do not change sitekey!
+
+?>
